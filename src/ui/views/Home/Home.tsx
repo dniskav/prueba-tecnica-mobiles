79d51b31b80c05b8be917ea29f044ba4011544a1
Dniskav <dniskav@gmail.com>
2025-03-24 00:17:49 +0100

    Feat: refactor

--- src/ui/views/Home/Home.tsx
+++ src/ui/views/Home/Home.tsx
@@ -1,33 +1,10 @@
-import { useEffect, useState } from 'react'
-import { Header, SearchBoxApi } from '../../components'
-import { ProductListItem } from '../../../modules/product/domain/Product'
-import { List } from '..'
-import { useProductStore } from '../../stores/useProductStore'
+import { Header } from '../../components'
 import { Outlet } from 'react-router-dom'
 
 export function Home() {
-  const fetchProducts = useProductStore((s) => s.fetchProducts)
-  const [filter, setFilter] = useState<string | undefined>()
-  const products = useProductStore((s) => s.products)
-  const loading = useProductStore((s) => s.loading)
-  const error = useProductStore((s) => s.error)
-
-  useEffect(() => {
-    fetchProducts(filter)
-  }, [filter])
-
-  const setQuery = (query: string) => {
-    setFilter(query)
-  }
-
   return (
     <>
       <Header />
-
-      <SearchBoxApi
-        quantity={!!filter ? products.length : 0}
-        result={(p: ProductListItem) => setQuery(p)}
-      />
 
       <Outlet />
     </>
--- src/ui/views/List/List.tsx
+++ src/ui/views/List/List.tsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from 'react'
+import { SearchBoxApi } from '../../components'
 import { Grid } from '../../components/Grid/Grid'
 import { useProductStore } from '../../stores/useProductStore'
+import { ProductListItem } from '../../../modules/product/domain/Product'
 
 export function List() {
+  const [filter, setFilter] = useState<string | undefined>()
+  const fetchProducts = useProductStore((s) => s.fetchProducts)
   const products = useProductStore((s) => s.products)
-  return <Grid items={products} />
+
+  useEffect(() => {
+    fetchProducts(filter)
+  }, [filter])
+  const setQuery = (query: string) => {
+    setFilter(query)
+  }
+  return (
+    <>
+      <SearchBoxApi
+        quantity={!!filter ? products.length : 0}
+        result={(p: ProductListItem) => setQuery(p)}
+      />
+
+      <Grid items={products} />
+    </>
+  )
 }
