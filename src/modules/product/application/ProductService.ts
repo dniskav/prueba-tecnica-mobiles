0549a03be0c261cd1487fdeb1097b633a96d3913
Dniskav <dniskav@gmail.com>
2025-03-23 23:33:41 +0100

    Fix: repository paths

--- src/modules/product/application/ProductService.ts
+++ src/modules/product/application/ProductService.ts
@@ -2,27 +2,17 @@ import { ProductListItem, ProductDetail } from '../domain/Product'
 import { ProductRepository } from '../infrastructure/ProductRepository'
 
 export const ProductService = {
-  /**
-   * Get all products from the repository
-   * @returns {Promise<ProductListItem[]>} List of products
-   */
-  getAllProducts: async (): Promise<ProductListItem[]> => {
-    return ProductRepository.getAll()
+  getAll: async (search: string | undefined): Promise<ProductListItem[]> => {
+    return ProductRepository.getAll(search)
   },
 
-  /**
-   * Get a product by ID and apply transformations if necessary
-   * @param {string} productId - The product ID
-   * @returns {Promise<ProductDetail | null>} Detailed product data or null if not found
-   */
-  getProductById: async (productId: string): Promise<ProductDetail | null> => {
+  getById: async (productId: string): Promise<ProductDetail | null> => {
     const product = await ProductRepository.getById(productId)
     if (!product) return null
 
-    // Example: Add custom logic if needed (e.g., format price, add computed values)
     return {
       ...product,
-      formattedPrice: `$${product.basePrice.toFixed(2)}` // Example of a computed property
+      formattedPrice: `$${product.basePrice.toFixed(2)}`
     }
   }
 }
