e3858d3b5bc31ca3628cee4d8eaa7f6057d912e3
Dniskav <dniskav@gmail.com>
2025-03-31 23:39:46 +0200

    Feat: cart icon items

--- src/ui/components/CartIcon/CartIcon.tsx
+++ src/ui/components/CartIcon/CartIcon.tsx
@@ -1,4 +1,5 @@
 import cartIcon from '/Bagicon.svg'
+import cartIconFilled from '/BagiconF.svg'
 import styles from './cartIcon.module.css'
 import { useProductContext } from '../../stores/productContext'
 
@@ -7,10 +8,13 @@ export function CartIcon() {
   const { state } = useProductContext()
   const cartCount = state.cart.length
 
+  // Usar el icono de carrito lleno si hay elementos en el carrito
+  const iconToUse = cartCount > 0 ? cartIconFilled : cartIcon
+
   return (
     <div className={styles.container}>
       <span className={styles.icon}>
-        <img src={cartIcon} alt="Cart" />
+        <img src={iconToUse} alt="Cart" />
       </span>
 
       <span className={styles.badge}>{cartCount}</span>
